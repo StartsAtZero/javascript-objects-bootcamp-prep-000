26e2ed41bed21681091a5a1515a34259df42c266
Alevtina Lobova <ally1125@gmail.com>
2018-01-20 01:23:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(playlist, artistName, songTitle){
  return Object.assign({},playlist, {[artistName]: songTitle})
 }
 updatePlaylist(playlist, "Beyonce","Mine")
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName
+}
+removeFromPlaylist(playlist,"Madonna")
